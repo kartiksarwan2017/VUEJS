c11d54c14b0479e755ae1073589140c38516461e
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2025-01-08 20:14:38 +0530

    Binding Inline Styles in Vuejs

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,32 @@ export default {
       channel: "<b>Codevolution</b>",
       hack: `<a href="#" onclick="alert('You have been hacked!')">Win a Prize!</a>`,
       headingId: "heading",
-      isDisabled: false
+      isDisabled: false, 
+      status: "success",
+      isPromoted: true,
+      isSoldOut: true,
+      highlightColor: "orange",
+      headerSize: 50,
+      headerStyleObject: {
+        color: "orange",
+        fontSize: "50px",
+        padding: "20px"
+      },
+      baseStyleObject: {
+        fontSize: "50px",
+        padding: "10px"
+      },
+      successStyleObject: {
+        color: "green",
+        backgroundColor: "lightgreen",
+        border: "1px solid green",
+        padding: "20px"
+      },
+      dangerStyleObject: {
+        color: "darkred",
+        backgoundColor: "red",
+        border: "1px solid darkred"
+      }
     };
   }
 }
@@ -28,8 +53,51 @@ export default {
 
     <h2 v-bind:id="headingId">Heading</h2>
     <button v-bind:disabled="isDisabled">Bind</button>
+
+    <h2 class="underline">Underlined Text</h2>
+
+    <h2 class="underline" v-bind:class="status">Status</h2>
+
+    <h2 v-bind:class="{'promoted' : isPromoted}">Promoted Movie</h2>
+
+    <h2 v-bind:class="isSoldOut ? 'sold-out' : 'new'">Soldout? Movie</h2>
+
+    <h2 v-bind:class="['new', 'promoted']">Newly Promoted Movie</h2>
+
+    <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out' : 'new']">Array Conditional Movie</h2>
+
+    <h2 v-bind:class="{
+      'promoted': isPromoted,
+      'new': !isSoldOut,
+      'sold-out': isSoldOut
+    }">Object Conditional Movie</h2>
+
+    <!-- <h2 v-bind:style="{color: highlightColor, 'font-size': headerSize + 'px'}">Inline Styles</h2> -->
+    <h2 v-bind:style="{color: highlightColor, fontSize: headerSize + 'px', padding: '20px'}">Inline Styles</h2>
+
+    <h2 v-bind:style="headerStyleObject">Style Object</h2>
+
+    <div v-bind:style="[baseStyleObject, successStyleObject]">Success Styles</div>
+
+  <div v-bind:style="[baseStyleObject, dangerStyleObject]">Danger Styles</div>
+
   </div>
 </template>
 
 <style scoped>
+.underline {
+  text-decoration: underline;
+}
+
+.promoted {
+  font-style: italic;
+}
+
+.new {
+  color: olivedrab;
+}
+
+.sold-out {
+  color: red;
+}
 </style>
